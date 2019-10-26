ffb46ef71d1433ce4eb25cd2bf9fd769d9d2ae5f
matthew29tang <matthew29tang@gmail.com>
2019-10-26 14:21:29 -0700

    Speech flac --> text --> translated done

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,69 @@
-var port = process.env.PORT || 4000; //sets local server port to 4000. 
-var express = require('express'); // Express web server framework
+var port = process.env.PORT || 4000;  
+var express = require('express'); 
+const fs = require('fs');
+var cors = require('cors');
+const speech = require('@google-cloud/speech');
+const { Translate } = require('@google-cloud/translate');
+const translate = new Translate({ projectId: 'subgen' });
 var app = express();
+app.use(cors());
 console.log("Starting up server...");
 
-app.get('//', function (req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.send({sub: "text"})
+var syncRecognizeWords = (filename, encoding, sampleRateHertz, languageCode, res) => {
+  const client = new speech.SpeechClient();
+  const config = {
+    enableWordTimeOffsets: true,
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+    enableAutomaticPunctuation: true,
+  };
+  const audio = {
+    content: fs.readFileSync(filename).toString('base64'),
+  };
+  const request = {
+    config: config,
+    audio: audio,
+  };
+
+  return client.recognize(request).then(raw => transcribe(raw, res));
+}
+
+var transcribe = (raw, res) => {
+  const [response] = raw
+  response.results.forEach(raw => {
+    result = []
+    console.log(`Transcription: `, raw.alternatives[0].transcript);
+    raw.alternatives[0].words.forEach(wordInfo => {
+      const startSecs = `${wordInfo.startTime.seconds}` + `.` + wordInfo.startTime.nanos / 100000000;
+      const endSecs = `${wordInfo.endTime.seconds}` + `.` + wordInfo.endTime.nanos / 100000000;
+      result.push({
+        raw: wordInfo.word,
+        start: startSecs,
+        end: endSecs
+      })
+    });
+    var count = 0;
+    result.forEach(word => {
+      translate.translate(word.raw, 'en').then(translated => {
+        word.text = translated[0];
+        count += 1
+        if (count === result.length) {
+          console.log(result);
+          res.send(result);
+        }
+      });
+    });
+  });
+}
+
+app.get('/', function (req, res) {
+  const filename = './video2.flac';
+  const encoding = 'FLAC';
+  const sampleRateHertz = 44100;
+  const languageCode = 'fr';
+  const result = syncRecognizeWords(filename, encoding, sampleRateHertz, languageCode, res);
 });
 
 
-app.listen(port, function () { }); //starts the server
+app.listen(port, function () { }); 
