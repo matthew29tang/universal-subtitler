132ea408b17c9a874ba16067b61d0ab6636f3774
matthew29tang <matthew29tang@gmail.com>
2019-10-26 15:08:20 -0700

    find sample rate

--- backend/download.js
+++ backend/download.js
@@ -2,25 +2,26 @@ const https = require('https');
 const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
 
-const file = fs.createWriteStream("file.mp4");
-const request = https.get("https://gcs-vimeo.akamaized.net/exp=1572127812~acl=%2A%2F585965178.mp4%2A~hmac=8b8ac70751c7aad03ebbe4eab8bf01390d874677df87eeea8722143f66dd626b/vimeo-prod-skyfire-std-us/01/908/7/179543978/585965178.mp4", function(response) {
-  response.pipe(file);
+const file = fs.createWriteStream("raw.mp4");
+const url = "https://gcs-vimeo.akamaized.net/exp=1572127812~acl=%2A%2F585965178.mp4%2A~hmac=8b8ac70751c7aad03ebbe4eab8bf01390d874677df87eeea8722143f66dd626b/vimeo-prod-skyfire-std-us/01/908/7/179543978/585965178.mp4";
+const request = https.get(url, function (response) {
+    response.pipe(file);
 });
 
-//const proc = new ffmpeg({source:"file.mp4"});
-//proc.setFfmpegPath('/Applications/ffmpeg');
-//proc.saveToFile("f.mp3", (stdout, stderr) => err ? console.log(stderr) : console.log('done'));
-
-file.on('finish', () => {ffmpeg('./file.mp4')
-.toFormat('flac')
-.on('error', (err) => {
-    console.log('An error occurred: ' + err.message);
-})
-.on('progress', (progress) => {
-    // console.log(JSON.stringify(progress));
-    console.log('Processing: ' + progress.targetSize + ' KB converted');
-})
-.on('end', () => {
-    console.log('Processing finished !');
-})
-.save('./hello.flac')});//path where you want to save your file
+file.on('finish', () => {
+    ffmpeg('./raw.mp4').toFormat('flac').on('error', (err) => {
+        console.log('An error occurred: ' + err.message);
+    }).on('progress', (progress) => {
+        // console.log(JSON.stringify(progress));
+        console.log('Processing: ' + progress.targetSize + ' KB converted');
+    }).on('end', () => {
+        console.log('Processing finished !');
+    }).save('./output.flac').ffprobe('./output.flac', (err, metadata) => {
+        var sampleRate = null;
+        metadata.streams.forEach(function (stream) {
+            if (stream.codec_type === "audio")
+                sampleRate = stream.sample_rate;
+        });
+        console.log("Sample rate: ", sampleRate);
+    });
+});
